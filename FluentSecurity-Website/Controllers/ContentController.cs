77458806828652d4cb72f3455e4baf38d2976e1e
Kristoffer Ahl <git@77dynamite.com>
2011-05-22 22:31:33 +0200

    Added layout, theme and images. Added a ContentController and actions Index, GettingStarted.

---
+++ FluentSecurity-Website/Controllers/ContentController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace FluentSecurity_Website.Controllers
+{
+    public class ContentController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult GettingStarted()
+		{
+			return View();
+		}
+    }
+}
--- FluentSecurity-Website/Global.asax.cs
+++ FluentSecurity-Website/Global.asax.cs
@@ -24,7 +24,7 @@ namespace FluentSecurity_Website
 			routes.MapRoute(
 				"Default", // Route name
 				"{controller}/{action}/{id}", // URL with parameters
-				new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+				new { controller = "Content", action = "Index", id = UrlParameter.Optional } // Parameter defaults
 			);
 
 		}
