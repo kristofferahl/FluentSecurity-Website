70dd346e3f3bc5dadb51ab4b66711c0e446a3222
Kristoffer Ahl <git@77dynamite.com>
2011-11-23 22:17:05 +0100

    Fixed issue with twitter date time stamps.

--- FluentSecurity-Website/App/Extensions/DateTimeExtensions.cs
+++ FluentSecurity-Website/App/Extensions/DateTimeExtensions.cs
@@ -6,17 +6,21 @@ namespace FluentSecurity.Website.App.Extensions
 {
 	public static class DateTimeExtensions
 	{
-		public static MvcHtmlString TimePastSince(this DateTime dateTime, int timeZoneDifferenceInHours, string daysTextFormat, string dayTextFormat, string hoursTextFormat, string hourTextFormat)
+		public static MvcHtmlString TimePastSince(this DateTime dateTime, int timeZoneDifferenceInHours, string daysTextFormat, string dayTextFormat, string hoursTextFormat, string hourTextFormat, string justNowText)
 		{
 			var twitterTimeZoneNow = DateTime.Now.AddHours(timeZoneDifferenceInHours);
 			var timePastSinceTweet = twitterTimeZoneNow.Subtract(dateTime);
 			var timePastString = string.Empty;
 
 			var totalHours = timePastSinceTweet.TotalHours;
-			if (totalHours <= 1)
+			
+			if (totalHours < 1)
+				timePastString = justNowText;
+
+			if (totalHours >= 1 && totalHours < 2)
 				timePastString = string.Format(hourTextFormat, Math.Ceiling(totalHours));
 
-			if (totalHours > 1)
+			if (totalHours >= 2)
 				timePastString = string.Format(hoursTextFormat, Math.Ceiling(totalHours));
 
 			if (totalHours >= 24)
