c0bc554ffbceb85f2c2ee92146ea103e95217a65
Kristoffer Ahl <git@77dynamite.com>
2012-08-29 23:00:30 +0200

    - Improved styling of tweets. - Added support for toggeling of tweet details. - Displaying the tweet content as HTML. - Added support for deleting tweets. - Removed the db and added ignore.

--- TweetStore.SelfHost/Assets/Scripts/app.js
+++ TweetStore.SelfHost/Assets/Scripts/app.js
@@ -1,5 +1,26 @@
 ﻿function TweetsCtrl($scope, $http) {
 	$scope.tweets = [];
+	$scope.activeTweet = null;
+
+	$scope.isActiveTweet = function (tweet) {
+		return tweet.Id === $scope.activeTweet;
+	};
+
+	$scope.getStatus = function (tweet) {
+		return tweet.Private ? "private" : "public";
+	};
+
+	$scope.getAuthor = function (tweet) {
+		if (tweet.AuthorId) return tweet.AuthorId;
+		if (tweet.AuthorName) return tweet.AuthorName;
+		return "-";
+	};
+
+	$scope.getContent = function (tweet) {
+		if (tweet.TweetHtmlContent) return tweet.TweetHtmlContent;
+		if (tweet.TweetTextContent) return tweet.TweetTextContent;
+		return "-";
+	};
 
 	$scope.showTweet = function (tweet) {
 		var pairs = [];
@@ -13,6 +34,30 @@
 			}
 		}
 		return pairs;
+	};
+
+	$scope.toggle = function (tweet) {
+		if ($scope.isActiveTweet(tweet)) {
+			$scope.activeTweet = null;
+		} else {
+			$scope.activeTweet = tweet.Id;
+		}
+	};
+
+	$scope.remove = function (tweet) {
+		var url = '/api/tweets/' + tweet.Id;
+		console.log(url);
+		$http.delete(url).success(function () {
+			for (var i=0; i < $scope.tweets.length; i++) {
+				if ($scope.tweets[i].Id === tweet.Id) {
+					$scope.tweets.splice(i, 1);
+					if ($scope.activeTweet === tweet.Id) {
+						$scope.activeTweet = null;
+					}
+					return;
+				}
+			}
+		});
 	};
 
 	$http.get('/api/tweets').success(function (data) {
--- TweetStore.SelfHost/TweetsController.cs
+++ TweetStore.SelfHost/TweetsController.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Web.Http;
+using System.Linq;
 
 namespace TweetStore.SelfHost
 {
@@ -7,7 +10,19 @@ namespace TweetStore.SelfHost
 	{
 		public IEnumerable<Tweet> GetAll()
 		{
-			return Program.Store.GetAll<Tweet>();
+			return Program.Store.GetAll<Tweet>(true);
+		}
+
+		[HttpDelete]
+		public Tweet Delete(Guid id)
+		{
+			var tweet = Program.Store.Query<Tweet>(true, t => t.Id == id).SingleOrDefault();
+			if (tweet != null)
+			{
+				Program.Store.Delete(id);
+				return tweet;
+			}
+			throw new HttpResponseException(HttpStatusCode.NotFound);
 		}
 	}
 }
