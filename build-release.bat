8b94f219d0be727a387e946614b804f62a569f92
Kristoffer Ahl <git@77dynamite.com>
2012-10-05 09:02:05 +0200

    - Added Build/Artifacts directory to .gitignore. - Added quotes to file path for build-*.bat.

--- build-release.bat
+++ build-release.bat
@@ -1,2 +1,2 @@
-powershell.exe -NoProfile -NonInteractive -InputFormat none -ExecutionPolicy unrestricted .\build-trigger.ps1 -Target Release
+powershell.exe -NoProfile -NonInteractive -InputFormat none -ExecutionPolicy unrestricted ".\build-trigger.ps1" -Target Release
 PAUSE
