6a64cad435914ebdf831f71833d31ebef03241d7
Kristoffer Ahl <git@77dynamite.com>
2013-06-14 10:53:10 +0200

    - Removed twitter search to temporarily fix issues with the website.

--- FluentSecurity-Website/App/Services/TwitterService.cs
+++ FluentSecurity-Website/App/Services/TwitterService.cs
@@ -27,60 +27,60 @@ namespace FluentSecurity.Website.App.Services
 
 		public IEnumerable<TweetListModel> GetByQuery(string searchterm, int amount)
 		{
-			using (var twitterCtx = new TwitterContext())
-			{
-				var searchResults = (twitterCtx.Search.Where(search =>
-					search.Type == SearchType.Search &&
-					search.PageSize == amount &&
-					search.Query == searchterm &&
-					search.WithRetweets == true
-					)).SingleOrDefault() ?? new Search();
+			//using (var twitterCtx = new TwitterContext())
+			//{
+			//	var searchResults = (twitterCtx.Search.Where(search =>
+			//		search.Type == SearchType.Search &&
+			//		search.PageSize == amount &&
+			//		search.Query == searchterm &&
+			//		search.WithRetweets == true
+			//		)).SingleOrDefault() ?? new Search();
 
-				foreach (var searchResult in searchResults.Entries)
-				{
-					var tweet = new TweetListModel
-					{
-						StatusId = searchResult.ID,
-						Published = searchResult.Published,
-						AuthorId = null,
-						AuthorName = searchResult.Author.Name,
-						AuthorUrl = searchResult.Author.URI,
-						TweetUrl = searchResult.Alternate,
-						TweetTextContent = searchResult.Title,
-						TweetHtmlContent = searchResult.Content
-					};
-					_tweetStore.Store(tweet);
-				}
-			}
+			//	foreach (var searchResult in searchResults.Entries)
+			//	{
+			//		var tweet = new TweetListModel
+			//		{
+			//			StatusId = searchResult.ID,
+			//			Published = searchResult.Published,
+			//			AuthorId = null,
+			//			AuthorName = searchResult.Author.Name,
+			//			AuthorUrl = searchResult.Author.URI,
+			//			TweetUrl = searchResult.Alternate,
+			//			TweetTextContent = searchResult.Title,
+			//			TweetHtmlContent = searchResult.Content
+			//		};
+			//		_tweetStore.Store(tweet);
+			//	}
+			//}
 			return _tweetStore.Query<TweetListModel>(false, t => t.TweetTextContent.Contains(searchterm));
 		}
 
 		public IEnumerable<TweetListModel> GetTweetsFor(string twitterId, int amount)
 		{
 			var trimmedTwitterId = twitterId.Replace("@", "");
-			using (var twitterCtx = new TwitterContext())
-			{
-				var searchResults = (twitterCtx.Status.Where(search =>
-					search.Type == StatusType.User &&
-					search.ID == trimmedTwitterId
-					)).Take(amount);
+			//using (var twitterCtx = new TwitterContext())
+			//{
+			//	var searchResults = (twitterCtx.Status.Where(search =>
+			//		search.Type == StatusType.User &&
+			//		search.ID == trimmedTwitterId
+			//		)).Take(amount);
 
-				foreach (var searchResult in searchResults)
-				{
-					var tweet = new TweetListModel
-					{
-						StatusId = searchResult.StatusID,
-						Published = searchResult.CreatedAt,
-						AuthorId = searchResult.ID,
-						AuthorName = searchResult.User.Name,
-						AuthorUrl = "http://twitter.com/"+ searchResult.ID,
-						TweetTextContent = searchResult.Text,
-						TweetHtmlContent = null,
-						TweetUrl = "http://twitter.com/" + searchResult.ID + "/status/" +  searchResult.StatusID,
-					};
-					_tweetStore.Store(tweet);
-				}
-			}
+			//	foreach (var searchResult in searchResults)
+			//	{
+			//		var tweet = new TweetListModel
+			//		{
+			//			StatusId = searchResult.StatusID,
+			//			Published = searchResult.CreatedAt,
+			//			AuthorId = searchResult.ID,
+			//			AuthorName = searchResult.User.Name,
+			//			AuthorUrl = "http://twitter.com/"+ searchResult.ID,
+			//			TweetTextContent = searchResult.Text,
+			//			TweetHtmlContent = null,
+			//			TweetUrl = "http://twitter.com/" + searchResult.ID + "/status/" +  searchResult.StatusID,
+			//		};
+			//		_tweetStore.Store(tweet);
+			//	}
+			//}
 			return _tweetStore.Query<TweetListModel>(false, t => t.AuthorId.Contains(trimmedTwitterId));
 		}
 	}
