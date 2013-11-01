501f091b96db88e24ee448b34508aa5d48b3d647
Kristoffer Ahl <git@77dynamite.com>
2013-11-01 17:08:25 +0100

    - Ensuring we don't cache the result when loading a wiki page fails.

--- FluentSecurity-Website/Controllers/WikiController.cs
+++ FluentSecurity-Website/Controllers/WikiController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using Kiwi.Markdown;
 
 namespace FluentSecurity.Website.Controllers
 {
@@ -16,7 +19,26 @@ namespace FluentSecurity.Website.Controllers
 				return RedirectPermanent("/" + WikiRoutePrefix + newDocId);
 			}
 
-			return View(MvcApplication.MarkdownService.GetDocument(docId));
+			Document document;
+			try
+			{
+				document = MvcApplication.MarkdownService.GetDocument(docId);
+			}
+			catch (NullReferenceException)
+			{
+				document = new Document
+				{
+					Title = "The wikipage is unavailable",
+					Content = "<h1>The wikipage is unavailable at this moment!</h1><p>Github might be down and we currently have no backup of the page. </strong>Try again in a little while!</strong></p>"
+				};
+				Response.Cache.SetExpires(DateTime.UtcNow.AddDays(-1));
+				Response.Cache.SetValidUntilExpires(false);
+				Response.Cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
+				Response.Cache.SetCacheability(HttpCacheability.NoCache);
+				Response.Cache.SetNoStore();
+			}
+
+			return View(document);
 		}
 	}
 }
